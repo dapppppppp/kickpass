aef664f1e2cda3bc34b4b7c127f8d74dbf75ce77
Dyinastie <dyinastiemp@gmail.com>
2024-12-10 21:43:28 +0700

    Update buy_tickect_screen

--- lib/screens/buy_ticket_screen.dart
+++ lib/screens/buy_ticket_screen.dart
@@ -1,20 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:kickpass/list/ticket_list.dart';
+import 'match_details_screen.dart';
 
-class BuyTicketScreen extends StatelessWidget {
+class BuyTicketScreen extends StatefulWidget {
   const BuyTicketScreen({super.key});
+
+  @override
+  _BuyTicketScreenState createState() => _BuyTicketScreenState();
+}
+
+class _BuyTicketScreenState extends State<BuyTicketScreen> {
+  List<Map<String, String>> filteredMatches = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredMatches = ticketList;
+  }
+
+  void _filterMatches(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        filteredMatches = ticketList;
+      } else {
+        filteredMatches = ticketList
+            .where((match) =>
+                match["title"]!.toLowerCase().contains(query.toLowerCase()) ||
+                match["location"]!.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Buy Ticket'),
-        backgroundColor:  Colors.blue,
+        backgroundColor: Colors.blue,
       ),
-      body: const Center(
-        child: Text(
-          'Daftar Tiket yang Tersedia',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
+      body: Column(
+        children: [
+          // Kolom pencarian
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari pertandingan atau stadion...',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              onChanged: _filterMatches,
+            ),
+          ),
+          // List pertandingan
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredMatches.length,
+              itemBuilder: (context, index) {
+                final match = filteredMatches[index];
+                return GestureDetector(
+                  onTap: () {
+                    // Pindah ke halaman MatchDetailsScreen saat Card di-tap
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MatchDetailsScreen(
+                          match: match,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            match["location"] ?? "",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          const Divider(thickness: 1),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  match["title"]?.split(" vs ")[0] ?? "",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                              const Text(
+                                "VS",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  match["title"]?.split(" vs ")[1] ?? "",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Divider(thickness: 1),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Tanggal",
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              Text(
+                                match["date"] ?? "",
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Waktu",
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              Text(
+                                match["time"] ?? "",
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
