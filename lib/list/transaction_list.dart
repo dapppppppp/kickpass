e22a8f86bc89e29916c23a7b19942a5fcce96a4b
Dyinastie <dyinastiemp@gmail.com>
2024-12-11 13:28:16 +0700

    Update transactions_screen

---
+++ lib/list/transaction_list.dart
@@ -0,0 +1,35 @@
+final List<Map<String, String>> transactionList = [
+  {
+    "id_transaksi": "12345678",
+    "title": "Team A vs Team B",
+    "date": "22 Desember 2024",
+    "time": "18:00 WIB",
+    "location": "Stadion Utama",
+    "category": "Tribun",
+    "price": "Rp 50.000",
+    "status": "Valid",
+    "kode_tiket": "F85Y"
+  },
+  {
+    "id_transaksi": "87654321",
+    "title": "Team C vs Team D",
+    "date": "20 Desember 2024",
+    "time": "20:00 WIB",
+    "location": "Stadion Merdeka",
+    "category": "VIP",
+    "price": "Rp 200.000",
+    "status": "Valid",
+    "kode_tiket": "YH8E"
+  },
+  {
+    "id_transaksi": "34512687",
+    "title": "Team E vs Team F",
+    "date": "10 Desember 2024",
+    "time": "16:00 WIB",
+    "location": "Stadion Jaya Raya",
+    "category": "Tribun",
+    "price": "Rp 55.000",
+    "status": "Expired",
+    "kode_tiket": "S881"
+  },
+];
--- lib/screens/transactions_screen.dart
+++ lib/screens/transactions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kickpass/list/transaction_list.dart';
+import 'package:kickpass/list/user_details.dart';
+import 'package:kickpass/screens/transaction_details_screen.dart';
 
 class TransactionsScreen extends StatelessWidget {
   const TransactionsScreen({super.key});
@@ -7,29 +10,166 @@ class TransactionsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Transaksi Kita'),
-        backgroundColor: Colors.blue,
+        title: const Text('Tiket Saya'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: [
-          _buildTransactionCard('Pertandingan 1', 'Berhasil', Icons.check_circle, Colors.green),
-          _buildTransactionCard('Pertandingan 2', 'Gagal', Icons.cancel, Colors.red),
-          _buildTransactionCard('Pertandingan 3', 'Pending', Icons.hourglass_empty, Colors.orange),
-        ],
-      ),
-    );
-  }
+      body: ListView.builder(
+        itemCount: transactionList.length,
+        itemBuilder: (context, index) {
+          final transaction = transactionList[index];
+          return GestureDetector(
+            onTap: () {
+              // Pindah ke halaman TransactionDetailsScreen saat Card di-tap
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TransactionDetailsScreen(
+                    transaction: transaction,
+                  ),
+                ),
+              );
+            },
+            child: Card(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Lokasi pertandingan
+                    Text(
+                      transaction["location"] ?? "",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const Divider(thickness: 1),
+                    const SizedBox(height: 8),
 
-  Widget _buildTransactionCard(String title, String status, IconData icon, Color color) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      elevation: 4,
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      child: ListTile(
-        leading: Icon(icon, color: color, size: 40),
-        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-        subtitle: Text('Status: $status'),
+                    // Nama tim dan VS
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            transaction["title"]?.split(" vs ")[0] ?? "",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        const Text(
+                          "VS",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Expanded(
+                          child: Text(
+                            transaction["title"]?.split(" vs ")[1] ?? "",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Divider(thickness: 1),
+                    const SizedBox(height: 8),
+
+                    // Tanggal dan waktu pertandingan
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Tanggal",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Text(
+                          transaction["date"] ?? "",
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Waktu",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Text(
+                          transaction["time"] ?? "",
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 8),
+                    const Divider(thickness: 1),
+                    const SizedBox(height: 8),
+
+                    // Nama pembeli
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Nama Pembeli",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Text(
+                          user.name,
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 4),
+
+                    // Status tiket
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Status Tiket",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 4, horizontal: 8),
+                          decoration: BoxDecoration(
+                            color: transaction["status"] == "Valid"
+                                ? Colors.green
+                                : Colors.red,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Text(
+                            transaction["status"] ?? "",
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
